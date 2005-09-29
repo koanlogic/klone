93bd4c6c9e71b6217bde8495de766bba61419e6d
tat <tat>
2005-09-29 12:13:41 +0000

    using __VAR_ARGS__ in macros body when ... is found in macros definition

--- klone/debug.h
+++ klone/debug.h
@@ -18,17 +18,17 @@ extern const char *WARN_LABEL;
  *
  * You can redefine this macro to redirect debug output.
  */
-#define output_message(label, args...) \
-    u_write_debug_message(label, __FILE__, __LINE__,__FUNCTION__, args) 
+#define output_message(label, ...) \
+    u_write_debug_message(label, __FILE__, __LINE__,__FUNCTION__, __VA_ARGS__) 
 
 
 /** \brief output a debug message */
-#define msg(label, args...) output_message(label, args)
+#define msg(label, ...) output_message(label, __VA_ARGS__)
 
 /** \brief output a debug message */
 #define msg_noargs(label, literal) output_message(label, "%s", literal)
 
-#define msg_err(label, args...) do { msg(label, args); goto err; } while(0)
+#define msg_err(label, ...) do { msg(label, __VA_ARGS__); goto err; } while(0)
 
 /** \brief write a debug message if \e expr not zero and enter the if-block
  *
@@ -112,36 +112,38 @@ extern const char *WARN_LABEL;
 
 /* cmsg_ macros 
    (console: print to stderr) */
-#define cmsg(args...)                   \
-    do { fprintf(stderr, args); fprintf(stderr, "\n"); } while(0)
-#define cmsg_err(args...)               do { cmsg(args); goto err; } while(0)
+#define cmsg(...)                   \
+    do { fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); } while(0)
+#define cmsg_err(...)               do { cmsg(__VA_ARGS__); goto err; } while(0)
 #define cmsg_err_if(expr)     \
     do { if( (expr) ) { cmsg("%s", #expr); goto err; } } while(0)
-#define cmsg_err_ifm(expr, args...)     \
-    do { if( (expr) ) { cmsg(args); goto err; } } while(0)
+#define cmsg_err_ifm(expr, ...)     \
+    do { if( (expr) ) { cmsg(__VA_ARGS__); goto err; } } while(0)
 
 /* warn_ macros */
-#define warn(args...)                   msg(WARN_LABEL, args)
-#define warn_err(args...)               msg_err(WARN_LABEL, args)
+#define warn(...)                       msg(WARN_LABEL, __VA_ARGS__)
+#define warn_err(...)                   msg_err(WARN_LABEL, __VA_ARGS__)
 #define warn_ifb(expr)                  msg_ifb(WARN_LABEL, expr)
 #define warn_if(expr)                   msg_if(WARN_LABEL, expr) 
 #define warn_return_if(expr, err)       msg_return_if(WARN_LABEL, expr, err)
 #define warn_err_if(expr)               msg_err_if(WARN_LABEL, expr)
-#define warn_err_ifm(expr, args...)     msg_err_ifm(WARN_LABEL, expr, args)
+#define warn_err_ifm(expr, ...)         \
+    msg_err_ifm(WARN_LABEL, expr, __VA_ARGS__)
 #define warn_goto_if(expr, gt)          msg_goto_if(WARN_LABEL, expr, gt)
 #define warn_strerror(errno)            msg_strerror(WARN_LABEL, errno)
 
 /* dbg_ macros */
 #ifndef NDEBUG
-    #define dbg(args...)                msg(DEBUG_LABEL, args)
-    #define dbg_err(args...)            msg_err(DEBUG_LABEL, args)
+    #define dbg(...)                    msg(DEBUG_LABEL, __VA_ARGS__)
+    #define dbg_err(...)                msg_err(DEBUG_LABEL, __VA_ARGS__)
     #define dbg_ifb(expr)               msg_ifb(DEBUG_LABEL, expr)
     #define dbg_if(expr)                msg_if(DEBUG_LABEL, expr) 
     #define dbg_return_if(expr, err)    msg_return_if(DEBUG_LABEL, expr, err)
     #define dbg_return_sif(expr, err)   msg_return_sif(DEBUG_LABEL, expr, err)
     #define dbg_err_if(expr)            msg_err_if(DEBUG_LABEL, expr)
     #define dbg_err_sif(expr)           msg_err_sif(DEBUG_LABEL, expr)
-    #define dbg_err_ifm(expr, args...)  msg_err_ifm(DEBUG_LABEL, expr, args)
+    #define dbg_err_ifm(expr, ...)      \
+        msg_err_ifm(DEBUG_LABEL, expr, __VA_ARGS__)
     #define dbg_goto_if(expr, gt)       msg_goto_if(DEBUG_LABEL, expr, gt)
     #define dbg_strerror(errno)         msg_strerror(DEBUG_LABEL, errno)
     /* simple debugging timing macros */
@@ -159,14 +161,14 @@ extern const char *WARN_LABEL;
     #include <ctype.h>
     /* this will be used just to avoid empty-if (and similar) warnings */
     #define dbg_nop()                   isspace(0)
-    #define dbg(args...)                dbg_nop()
-    #define dbg_err(args...)            do { goto err; } while(0)
+    #define dbg(...)                    dbg_nop()
+    #define dbg_err(...)                do { goto err; } while(0)
     #define dbg_ifb(expr)               if( (expr) )
     #define dbg_if(expr)                expr
     #define dbg_return_if(expr, err)    do { if( (expr) ) return err; } while(0)
     #define dbg_err_if(expr)            do { if( (expr)) goto err; } while(0)
     #define dbg_err_sif(expr)           do { if( (expr)) goto err; } while(0)
-    #define dbg_err_ifm(expr, args...)  do { if( (expr)) goto err; } while(0)
+    #define dbg_err_ifm(expr, ...)      do { if( (expr)) goto err; } while(0)
     #define dbg_goto_if(expr, gt)       do { if((expr)) goto gt; } while(0)
     #define dbg_strerror(errno)         dbg_nop()
     #define TIMER_ON
