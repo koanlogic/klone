66739c46da984228a0b6da0822c7c24187b6a19c
tho <tho>
2005-09-30 10:03:15 +0000

    [klone] added missing include <netinet/in.h>

--- klone/os.h
+++ klone/os.h
@@ -23,6 +23,7 @@
     #include <sys/types.h>
     #include <sys/socket.h>
     #include <sys/wait.h>
+    #include <netinet/in.h>
     #include <arpa/inet.h>
     #include <sys/un.h>
     #include <netdb.h>
