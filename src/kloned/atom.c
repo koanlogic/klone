6978787e295e0e3275d14d4759e35ae117bd104b
tho <tho>
2005-11-24 22:47:21 +0000

    [klone] kloned/atom.c args check

--- src/kloned/atom.c
+++ src/kloned/atom.c
@@ -5,7 +5,7 @@
  * This file is part of KLone, and as such it is subject to the license stated
  * in the LICENSE file which you have received as part of this distribution.
  *
- * $Id: atom.c,v 1.9 2005/11/24 22:26:04 tho Exp $
+ * $Id: atom.c,v 1.10 2005/11/24 22:47:21 tho Exp $
  */
 
 #include "klone_conf.h"
@@ -13,10 +13,14 @@
 #include <klone/utils.h>
 #include <klone/atom.h>
 
-int atom_create(const char *id, const char *data, size_t size, void* arg, 
+int atom_create(const char *id, const char *data, size_t size, void *arg, 
     atom_t **patom)
 {
     atom_t *atom = NULL;
+
+    dbg_err_if (id == NULL);
+    dbg_err_if (data == NULL);
+    dbg_err_if (patom == NULL);
 
     atom = u_zalloc(sizeof(atom_t));
     dbg_err_if(atom == NULL);
@@ -54,7 +58,7 @@ int atom_free(atom_t *atom)
 /* sum of atoms size field */
 size_t atoms_size(atoms_t *as)
 {
-    dbg_err_if(as == NULL);
+    dbg_err_if (as == NULL);
 
     return as->size;
 err:
@@ -64,7 +68,7 @@ err:
 /* # of atoms */
 size_t atoms_count(atoms_t *as)
 {
-    dbg_err_if(as == NULL);
+    dbg_err_if (as == NULL);
 
     return as->count;
 err:
@@ -77,7 +81,9 @@ int atoms_getn(atoms_t *as, size_t n, atom_t **patom)
     atom_t *atom;
     size_t i = 0;
 
-    dbg_err_if(as == NULL || n >= as->count || patom == NULL);
+    dbg_err_if (as == NULL);
+    dbg_err_if (n >= as->count);
+    dbg_err_if (patom == NULL);
 
     LIST_FOREACH(atom, &as->list, np)
     {
@@ -97,7 +103,9 @@ int atoms_get(atoms_t *as, const char *id, atom_t **patom)
 {
     atom_t *atom;
 
-    dbg_err_if(as == NULL || id == NULL || patom == NULL);
+    dbg_err_if (as == NULL);
+    dbg_err_if (id == NULL);
+    dbg_err_if (patom == NULL);
 
     LIST_FOREACH(atom, &as->list, np)
     {
@@ -115,7 +123,8 @@ err:
 /* add an atom to the list */
 int atoms_add(atoms_t *as, atom_t *atom)
 {
-    dbg_err_if(as == NULL || atom == NULL);
+    dbg_err_if (as == NULL);
+    dbg_err_if (atom == NULL);
 
     LIST_INSERT_HEAD(&as->list, atom, np);
 
@@ -130,7 +139,8 @@ err:
 /* add an atom to the list */
 int atoms_remove(atoms_t *as, atom_t *atom)
 {
-    dbg_err_if(as == NULL || atom == NULL);
+    dbg_err_if (as == NULL);
+    dbg_err_if (atom == NULL);
 
     LIST_REMOVE(atom, np);
 
@@ -148,7 +158,7 @@ int atoms_create(atoms_t **pas)
 {
     atoms_t *as = NULL;
 
-    dbg_err_if(pas == NULL);
+    dbg_err_if (pas == NULL);
 
     as = u_zalloc(sizeof(atoms_t));
     dbg_err_if(as == NULL);
@@ -163,24 +173,21 @@ err:
     return ~0;
 }
 
-
 /* free an atom list */
 int atoms_free(atoms_t *as)
 {
     atom_t *atom;
 
-    dbg_err_if(as == NULL);
-
-    while(atoms_count(as))
+    if (as)
     {
-        dbg_err_if(atoms_getn(as, 0, &atom));
-        dbg_err_if(atoms_remove(as, atom));
+        while(atoms_count(as))
+        {
+            dbg_err_if(atoms_getn(as, 0, &atom));
+            dbg_err_if(atoms_remove(as, atom));
+        }
+
+        U_FREE(as);
     }
 
-    U_FREE(as);
-
     return 0;
-err:
-    return ~0;
 }
-
