ce533b0733087178988ee7a7a351aef9802f4424
tat <tat>
2007-10-22 16:29:07 +0000

    [klone] trailing newline removed from client request

--- src/libpage/request.c
+++ src/libpage/request.c
@@ -5,7 +5,7 @@
  * This file is part of KLone, and as such it is subject to the license stated
  * in the LICENSE file which you have received as part of this distribution.
  *
- * $Id: request.c,v 1.41 2007/10/22 15:11:46 tat Exp $
+ * $Id: request.c,v 1.42 2007/10/22 16:29:07 tat Exp $
  */
 
 #include "klone_conf.h"
@@ -618,11 +618,17 @@ err:
  */
 int request_set_client_request(request_t *rq, const char *ln)
 {
+    char *p;
     dbg_err_if(rq == NULL);
     dbg_err_if(ln == NULL);
 
     rq->cli_rq = u_strdup(ln);
     dbg_err_if(rq->cli_rq == NULL);
+
+    /* cut the trailing newline if any */
+    for(p = rq->cli_rq; *p && (*p != '\r' && *p != '\n'); ++p)
+        continue;
+    *p = 0;
 
     return 0;
 err:
