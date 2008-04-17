eca6ab8de6662d0f25ff8b465f936b228c890972
tat <tat>
2008-04-17 10:49:04 +0000

    [klone] inglish fics

--- src/kloned/server.c
+++ src/kloned/server.c
@@ -5,7 +5,7 @@
  * This file is part of KLone, and as such it is subject to the license stated
  * in the LICENSE file which you have received as part of this distribution.
  *
- * $Id: server.c,v 1.61 2008/04/17 09:30:51 tat Exp $
+ * $Id: server.c,v 1.62 2008/04/17 10:49:04 tat Exp $
  */
 
 #include "klone_conf.h"
@@ -885,7 +885,7 @@ static int server_spawn_children(server_t *s)
             dbg_err_if(server_spawn_child(s, be));
             /* child context? */
             if(ctx->pipc)
-                break; /* the child previously spawn is dieing, exit */
+                break; /* the child previously spawned is dying, exit */
             be->fork_child--;
         }
     }
