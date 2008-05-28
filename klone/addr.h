25ac60678f542f5fe6f4a1253ab6d15fc7b50d45
tho <tho>
2008-05-28 16:38:30 +0000

    [klone] added explicit netinet include to addr.h

--- klone/addr.h
+++ klone/addr.h
@@ -5,11 +5,14 @@
  * This file is part of KLone, and as such it is subject to the license stated
  * in the LICENSE file which you have received as part of this distribution.
  *
- * $Id: addr.h,v 1.11 2006/05/27 16:34:01 tat Exp $
+ * $Id: addr.h,v 1.12 2008/05/28 16:38:30 tho Exp $
  */
 
 #ifndef _KLONE_ADDR_H_
 #define _KLONE_ADDR_H_
+
+#include <sys/types.h>
+#include <netinet/in.h>
 
 #include <u/libu.h>
 
