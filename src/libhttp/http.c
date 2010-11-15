koanlogic/klone

commit 6d141c34ab62128038541e9213b2ff9bd4f277b1

s/strncpy/u_strlcpy

src/libhttp/http.c
@@ -225,13 +225,12 @@ static int http_is_valid_uri(request_t *rq, const char *buf, size_t len)
     dbg_err_if (rq == NULL);
     dbg_err_if (buf == NULL);
 
-    dbg_err_if (len >= URI_MAX);
+    dbg_err_if (len >= sizeof uri);
 
     h = request_get_http(rq);
     dbg_err_if (h == NULL);
     
-    strncpy(uri, buf, len);
-    uri[len] = 0;
+    u_strlcpy(uri, buf, len);
 
     /* try the url itself */
     if(broker_is_valid_uri(h->broker, h, rq, uri, strlen(uri)))

src/libhttp/sup_emb.c
@@ -29,16 +29,15 @@ static int supemb_is_valid_uri(http_t *h, request_t *rq, const char *uri,
         size_t len, void **handle, time_t *mtime)
 {
     embres_t *e;
-    char filename[U_FILENAME_MAX] = { 0 };
+    char filename[U_FILENAME_MAX] = { '\0' };
 
     dbg_err_if (uri == NULL);
     dbg_err_if (mtime == NULL);
-    dbg_err_if (len >= U_FILENAME_MAX);
+    dbg_err_if (len >= sizeof filename);
 
     u_unused_args(h, rq);
 
-    strncpy(filename, uri, len);
-    filename[len] = 0;
+    u_strlcpy(filename, uri, len);
 
     if(emb_lookup(filename, &e) == 0)
     {   /* resource found */

klone/utils.h
@@ -30,14 +30,6 @@
 extern "C" {
 #endif
 
-#ifndef MIN
-#define MIN(a,b)    (a < b ? a : b)
-#endif
-
-#ifndef MAX
-#define MAX(a,b)    (a > b ? a : b)
-#endif
-
 #define KLONE_FREE(p) do {if (p) { free(p); p = NULL; }} while (0)
 
 #define klone_die(...) do { u_con(__VA_ARGS__); exit(EXIT_FAILURE); } while(0)
@@ -98,7 +90,9 @@ const mime_map_t* u_get_mime_map(const char *file_name);
 int u_match_ext(const char *filename, const char *extension);
 
 /* date time conversion funcs */
-int u_tt_to_rfc822(char dst[], time_t ts);
+#define RFC822_DATE_BUFSZ   32
+
+int u_tt_to_rfc822(char dst[RFC822_DATE_BUFSZ], time_t ts);
 int u_httpdate_to_tt(const char *str, time_t *tp);
 int u_rfc822_to_tt(const char *str, time_t *tp);
 int u_rfc850_to_tt(const char *str, time_t *tp);

src/kloned/entry.c
@@ -345,11 +345,11 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hPrevInst,
         /* set up service name and description reading from the config file */
         name = u_config_get_subkey_value(ctx->config, "daemon.name");
         if(name)
-            strncpy(ss_name, name, SS_NAME_BUFSZ);
+            u_strlcpy(ss_name, name, sizeof ss_name);
 
         desc = u_config_get_subkey_value(ctx->config, "daemon.description");
         if(desc)
-            strncpy(ss_desc, desc, SS_DESC_BUFSZ);
+            u_strlcpy(ss_desc, desc, sizeof ss_desc);
 
         if(ctx->serv_op == SERV_INSTALL)
             dbg_err_if(InstallService());

src/kloned/ppc_log_add.c
@@ -61,8 +61,7 @@ int server_ppc_cmd_log_add(server_t *s, int level, const char *str)
 
     la.bid = ctx->backend->id;
     la.level = level;
-    strncpy(la.log, str, U_MAX_LOG_LENGTH);
-    la.log[U_MAX_LOG_LENGTH -1] = 0;
+    u_strlcpy(la.log, str, sizeof la.log);
 
     /* send the command request */
     nop_err_if(ppc_write(s->ppc, ctx->pipc, PPC_CMD_LOG_ADD, (char*)&la, 

src/kloned/ppc_log_get.c
@@ -57,8 +57,7 @@ int server_ppc_cmd_log_get(server_t *s, size_t i, char *line)
     nop_err_if(plg.i < 0); /* error or eof */
 
     /* copy-out the line */
-    strncpy(line, plg.line, KLOG_LN_SZ);
-    line[KLOG_LN_SZ - 1] = 0;
+    u_strlcpy(line, plg.line, sizeof line);
 
     return 0;
 err:

src/kloned/server.c
@@ -301,7 +301,7 @@ static void server_watch_fd(server_t *s, int fd, unsigned int mode)
     if(mode & WATCH_FD_EXCP)
         FD_SET(fd, &s->exfds);
 
-    s->hfd = MAX(s->hfd, fd);
+    s->hfd = U_MAX(s->hfd, fd);
 }
 
 static void server_close_fd(server_t *s, int fd)

src/libcodec/cipher.c
@@ -38,7 +38,7 @@ static void codec_cbufcpy(codec_cipher_t *cc, char *dst, size_t *dcount)
 {
     size_t count;
 
-    count = MIN(*dcount, cc->ccount);
+    count = U_MIN(*dcount, cc->ccount);
 
     memcpy(dst, cc->cbuf + cc->coff, count);
     cc->ccount -= count;
@@ -115,7 +115,7 @@ static ssize_t cipher_transform(codec_t *codec, char *dst, size_t *dcount,
         /* the cbuf must be empty because we need the whole buffer to be sure to
            have enough output space for EVP_{Encrypt,Decrypt}Update */
 
-        c = MIN(src_sz, CODEC_CIPHER_MAX_INPUT);
+        c = U_MIN(src_sz, CODEC_CIPHER_MAX_INPUT);
 
         wr = -1; /* just used to return an int value */
         dbg_err_if(!cc->update(&cc->cipher_ctx, cc->cbuf, &wr, src, c));

commit b5dbcb492932fcb02d5b2523fb48db54188033b6

fix regression in http_is_valid_uri() introduced with commit 6d141c34ab6212803854

src/libhttp/http.c
@@ -225,12 +225,12 @@ static int http_is_valid_uri(request_t *rq, const char *buf, size_t len)
     dbg_err_if (rq == NULL);
     dbg_err_if (buf == NULL);
 
-    dbg_err_if (len >= sizeof uri);
+    dbg_err_if (len + 1 > sizeof uri);
 
     h = request_get_http(rq);
     dbg_err_if (h == NULL);
     
-    u_strlcpy(uri, buf, len);
+    u_strlcpy(uri, buf, len + 1);
 
     /* try the url itself */
     if(broker_is_valid_uri(h->broker, h, rq, uri, strlen(uri)))

src/libhttp/sup_cgi.c
@@ -182,16 +182,16 @@ static int cgi_is_valid_uri(http_t *h, request_t *rq, const char *uri,
 
     dbg_return_if (uri == NULL, 0);
     dbg_return_if (mtime == NULL, 0);
-    dbg_return_if (len >= U_FILENAME_MAX, 0);
+    dbg_return_if (len + 1 > U_FILENAME_MAX, 0);
 
     memcpy(fqn, uri, len);
-    fqn[len] = 0;
+    fqn[len] = '\0';
 
     /* fqn must be already normalized */
     if(strstr(fqn, ".."))
         return 0; 
     
-    if( stat(fqn, &st) == 0 && S_ISREG(st.st_mode))
+    if(stat(fqn, &st) == 0 && S_ISREG(st.st_mode))
     {
         /* if it's not a cgi given its extension of uri then exit */
         if(!cgi_ext(h, rq, fqn, NULL) && !cgi_script(h, rq, fqn))

src/libhttp/sup_emb.c
@@ -33,11 +33,11 @@ static int supemb_is_valid_uri(http_t *h, request_t *rq, const char *uri,
 
     dbg_err_if (uri == NULL);
     dbg_err_if (mtime == NULL);
-    dbg_err_if (len >= sizeof filename);
+    dbg_err_if (len + 1 > sizeof filename);
 
     u_unused_args(h, rq);
 
-    u_strlcpy(filename, uri, len);
+    u_strlcpy(filename, uri, len + 1);
 
     if(emb_lookup(filename, &e) == 0)
     {   /* resource found */

src/libhttp/sup_fs.c
@@ -25,12 +25,12 @@ static int fs_is_valid_uri(http_t *h, request_t *rq, const char *uri,
 
     dbg_return_if (uri == NULL, 0);
     dbg_return_if (mtime == NULL, 0);
-    dbg_return_if (len >= U_FILENAME_MAX, 0);
+    dbg_return_if (len + 1 > U_FILENAME_MAX, 0);
 
     u_unused_args(h);
 
     memcpy(fqn, uri, len);
-    fqn[len] = 0;
+    fqn[len] = '\0';
 
     /* fqn must be already normalized */
     if(strstr(fqn, ".."))

src/libhttp/sup_kilt.c
@@ -84,11 +84,11 @@ static int kilt_is_valid_uri(http_t *h, request_t *rq, const char *uri,
 
     dbg_return_if (uri == NULL, 0);
     dbg_return_if (mtime == NULL, 0);
-    dbg_return_if (len >= U_FILENAME_MAX, 0);
+    dbg_return_if (len + 1 > U_FILENAME_MAX, 0);
     dbg_return_if (url_to_ku == NULL, 0);
 
     memcpy(url, uri, len);
-    url[len] = 0;
+    url[len] = '\0';
 
     for(i = 0; i < kilt_nurls; ++i)
     {